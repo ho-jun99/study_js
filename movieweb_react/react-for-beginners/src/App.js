2e0d05b5184db9fd475647834c95f69db6fd903f
hozuu <38792466+ho-jun99@users.noreply.github.com>
2023-08-17 16:47:15 +0900

    [ReactJS로 영화 웹 서비스 만들기] ~ #6.3

--- movieweb_react/react-for-beginners/src/App.js
+++ movieweb_react/react-for-beginners/src/App.js
@@ -1,10 +1,29 @@
+import styles from "./css/App.module.css"
+import {useState,useEffect} from "react";
 
 function App() {
-  return (
-    <div>
-      <h2>Hello World!</h2>
-    </div>
-  );
+    const [counter,setCounter] = useState(0);
+    const [keyword, setKeyword] = useState("")
+    const onClick = () => setCounter((prev) => prev + 1);
+    console.log("every time will call !")
+    useEffect(() => {
+        console.log("CALLING API")
+    }, []);
+
+    useEffect(() => {
+        if ( keyword !== "") console.log("SEARCH FOR ", keyword)
+    }, [keyword]);
+    const onKeywordChg = (e) => {
+        setKeyword(e.target.value);
+        console.log(keyword)
+    }
+    return (
+        <div>
+            <input value={keyword} onChange={onKeywordChg} type={"text"} placeholder={"여기에 검색어를 입력해주세요"}/>
+            <h1 className={styles.title}>{counter}</h1>
+            <button onClick={onClick}>클릭클릭</button>
+        </div>
+    );
 }
 
 export default App;
---
+++ movieweb_react/react-for-beginners/src/Button.js
@@ -0,0 +1,12 @@
+import PropTypes from "prop-types";
+import styles from "./css/Button.module.css"
+function Button({text}) {
+    console.log("컴포넌트 버튼입니다")
+    return <button className={styles.btn}>{text}</button>
+}
+
+Button.prototype = {
+    text : PropTypes.string.isRequired,
+};
+
+export default Button;
