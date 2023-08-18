7a2a69d83e647ebe602d629ecd255d8e41594235
hozuu <38792466+ho-jun99@users.noreply.github.com>
2023-08-18 17:25:15 +0900

    [ReactJS로 영화 웹 서비스 만들기] ~ #7.2 Coin Tracker

--- movieweb_react/react-for-beginners/src/App.js
+++ movieweb_react/react-for-beginners/src/App.js
@@ -1,33 +1,27 @@
 import {useState, useEffect} from "react";
-function App() {
-    const [myTodo,setMyTodo] = useState("")
-    const [myTodos,setMyTodos] = useState([]);
-    const onChgTodoInput = (e) => {
-        setMyTodo(e.target.value);
-    };
-    const onTodoSubmit = (e) => {
-        e.preventDefault()
-        if (myTodo === "") return;
-        setMyTodo("");
-        setMyTodos((prevArray) => [...myTodos,myTodo])
-        console.log(myTodo)
-    }
 
-    console.log(myTodos)
-    return (
-        <>
-            <h1>CURRENT TODOS :  {myTodos.length}</h1>
-            <form onSubmit={onTodoSubmit}>
-                <input value={myTodo} onChange={onChgTodoInput} type={"text"} placeholder={"Write your to do"}></input>
-                <button type={"submit"}>확인</button>
-            </form>
-            <hr/>
-            <ul></ul>
-                {myTodos.map((item,index) => {
-                    return <li key={index+item} >{item}</li>
-                })}
-        </>
-    );
+const API_URL = "https://api.coinpaprika.com/v1/tickers";
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch(API_URL)
+        .then((res) => res.json())
+        .then((json) => {
+          setCoins(json)
+          setLoading(false)
+        });},
+      []);
+
+  return (
+    <>
+      <h1>The Coins! ({coins.length})</h1>
+      {loading ? <strong>now Loading...</strong> : null}
+      <ul>
+        {coins.map((coin,index) => <li key={index}>{coin.name} ({coin.symbol}) : {coin.quotes.USD.price}</li>)}
+      </ul>
+    </>
+  );
 }
 
 export default App;
