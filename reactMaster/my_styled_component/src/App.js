ae3e140f75f3ad5ea0a0131b362e5b80a51d87de
hozuu <38792466+ho-jun99@users.noreply.github.com>
2023-08-24 17:43:00 +0900

    .

--- reactMaster/my_styled_component/src/App.js
+++ reactMaster/my_styled_component/src/App.js
@@ -1,8 +1,23 @@
+import styled from "styled-components";
 function App() {
   return (
-    <div>
-    </div>
+    <Father style={{ display : "flex" }}>
+      <Box bgColor = "teal"><Text>안녕하세요!</Text></Box>
+      <Box bgColor = "tomato"></Box>
+    </Father>
   );
 }
 
 export default App;
+
+const Father = styled.div`
+  display: flex;
+`;
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
+  width: 100px;
+  height: 100px;
+`;
+const Text = styled.span`
+  color : white;
+`
